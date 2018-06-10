0fe1b281124561dee8ca7c2b41c96a3d6c92d588
Poliane Ketterine Valdrich <polianevaldrich@gmail.com>
2018-06-10 17:47:14 -0300

    aula final

--- config/express.js
+++ config/express.js
@@ -14,9 +14,27 @@ module.exports = function (){
     app.use(bodyParser.json());
     app.use(expressValidator());
 
+    app.use(function(req, res, next) {
+        res.status(404).render('erros/404');
+    });
+
     load('routes', {cwd: 'app'}) 
         .then('infra')
         .into(app);
+
+    app.use(function(req,res,next){
+        res.status(404).render('erros/404');
+        next();
+    });
+
+    app.use(function(error,req,res,next){
+        if(process.env.NODE_ENV == 'production') {
+            res.status(500).render('error/500');
+            return;
+        }
+        next(error);
+    
+    });    
 
     return app;
 }
